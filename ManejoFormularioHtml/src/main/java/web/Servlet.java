28f1508447073ea4cdce5771991ed16ed569eb17
Néstor Fabricio Parra <fabri0176@gmail.com>
2020-02-27 20:00:05 -0500

    fomurlarios servlets

--- ManejoFormularioHtml/src/main/java/web/Servlet.java
+++ ManejoFormularioHtml/src/main/java/web/Servlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.*;
  */
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF8");
@@ -29,19 +29,35 @@ public class Servlet extends HttpServlet {
         String ocupacion = request.getParameter("ocupacion");
         String musica[] = request.getParameterValues("musica");
         String comentarios = request.getParameter("comentarios");
-        
+
         out.print("<html>");
         out.print("<head>");
         out.print(" <title>Resultado Servlet</title>");
         out.print("</head>");
-        
+
         out.print("<body>");
         out.print("<h1>Parametros procesados por el servlet</h1>");
         out.print("<p>Usuario: " + usuario + "</p>");
         out.print("<p>Password: " + password + "</p>");
+
+        if (tecnologias != null) {
+            for (String tecnologia : tecnologias) {
+                out.print("<p>Tecnologia: "+tecnologia+"</p>");
+            }
+        }
         
+        if (musica != null) {
+            for (String generoMusical : musica) {
+                out.print("<p>Música: "+generoMusical+"</p>");
+            }
+        }
+        
+        out.print("Ocupación: "+ocupacion);
+        
+        out.print("Comentarios: "+comentarios);
+
         out.print("</body>");
-        
+
         out.print("</html>");
     }
 }
